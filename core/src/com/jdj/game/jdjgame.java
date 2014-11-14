30e9d6913312ed607dc6ec6d2b5111b8eff4e51a
DeepRaithatha <raithathadeep@gmail.com>
2014-11-14 13:37:04 -0500

    Rotation

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -21,7 +21,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-
+// Thanks to Matt Brock for helping
 //TESTING BRANCHES
 public class jdjgame extends ApplicationAdapter {
     UpButton upButton;
