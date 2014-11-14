8be6845a40243dc989452d723e34bc886793b411
JoseRivas <jose.rivas.1997@gmail.com>
2014-11-13 20:28:19 -0500

    OrthoCam

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -1,94 +1,106 @@
 package com.jdj.game;
+// Orthographic Camera + Gesture Listener
+//Source: http://www.gamefromscratch.com/post/2013/11/06/LibGDX-Tutorial-7-Camera-basics.aspx
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
 
-// Source: http://programmersweb.blogspot.ca/2012/07/simple-libgdx-box2d-bouncing-ball.html
-public class jdjgame extends ApplicationAdapter {
-    static final float BOX_STEP = 1 / 60f;
-    static final int velocity = 10; // set the velocity of the ball
-    static final int BOX_POSITION_ITERATIONS = 2;
-    static final float WORLD_TO_BOX = 0.01f;
-    static final float BOX_WORLD_TO = 100f;
-    World world = new World(new Vector2(0, -100), true);
-    Box2DDebugRenderer debugRenderer;
-    OrthographicCamera camera;
-    SpriteBatch batch;
-    Texture img;
-    Sprite sprite;
-    Body body;
+public class jdjgame implements ApplicationListener, GestureListener {
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
     @Override
     public void create() {
+        camera = new OrthographicCamera(1280, 720);
         batch = new SpriteBatch();
-        //sprite since it's going to move
-        img = new Texture("penguin.png");
-        sprite = new Sprite(img);
-        camera = new OrthographicCamera();
-        camera.viewportHeight = 320;
-        camera.viewportWidth = 480;
-        camera.position.set(camera.viewportWidth * .5f, camera.viewportHeight * .5f, 0f);
-        camera.update();
-        //Ground body
-        BodyDef groundBodyDef = new BodyDef();
-        groundBodyDef.position.set(new Vector2(0, 10));
-        Body groundBody = world.createBody(groundBodyDef);
-        PolygonShape groundBox = new PolygonShape(); // imaginary floor, where the ball is bouncing
-        groundBox.setAsBox((camera.viewportWidth) * 2, 10.0f);
-        groundBody.createFixture(groundBox, 0.0f);
-        //Dynamic Body
-        sprite.setPosition(camera.viewportWidth / 2, camera.viewportHeight / 2);
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2);
-        body = world.createBody(bodyDef);
-        CircleShape dynamicCircle = new CircleShape();
-        dynamicCircle.setRadius(5f); // radius of the circle/shape
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = dynamicCircle;
-        fixtureDef.density = 10.0f;
-        fixtureDef.friction = 0.0f;
-        fixtureDef.restitution = 1;
-        body.createFixture(fixtureDef);
-        debugRenderer = new Box2DDebugRenderer();
+        texture = new Texture(Gdx.files.internal("2048.jpg"));
+        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        sprite = new Sprite(texture);
+        sprite.setOrigin(0, 0);
+        sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
+        Gdx.input.setInputProcessor(new GestureDetector(this));
     }
     @Override
     public void dispose() {
+        batch.dispose();
+        texture.dispose();
     }
-
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        debugRenderer.render(world, camera.combined);
-
-        world.step(BOX_STEP, velocity, BOX_POSITION_ITERATIONS);
-        sprite.setPosition(body.getPosition().x, body.getPosition().y);
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(sprite, sprite.getX(), sprite.getY());
+        sprite.draw(batch);
         batch.end();
     }
     @Override
     public void resize(int width, int height) {
     }
-
     @Override
     public void pause() {
     }
-
     @Override
     public void resume() {
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+// TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+// TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+// TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+// TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        // TODO Auto-generated method stub
+        camera.translate(-deltaX, 0);
+        camera.update();
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+// TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+                         Vector2 pointer1, Vector2 pointer2) {
+// TODO Auto-generated method stub
+        return false;
     }
 }
