0e694793e10d464a1b0f1ba0a49ac6d77fdf2d31
JoseRivas <jose.rivas.1997@gmail.com>
2014-11-15 20:23:53 -0500

    Latest

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -14,6 +14,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -21,9 +23,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-
+import com.badlogic.gdx.math.Matrix4;
 public class jdjgame extends ApplicationAdapter {
-    final float PIXELS_TO_METERS = 100f;//convert pixels to meters since box2d uses meters
+    final float fPM = 100f;//convert pixels to meters since box2d uses meters
     Stage stage;
     TextButton button;
     TextButton.TextButtonStyle textButtonStyle;
@@ -32,49 +34,69 @@ public class jdjgame extends ApplicationAdapter {
     Skin skin;
     TextureAtlas buttonAtlas;
     SpriteBatch batch;
-    Sprite sprite;
-    Texture img;
+    Sprite sPeng, sGround;
+    Texture iPeng, iGround;
     World world;
     Body body;
     OrthographicCamera camera;
     int nWidth, nHeight;
+    Box2DDebugRenderer debugRenderer;
+    Matrix4 debugMatrix;
     @Override
     public void create() {
         batch = new SpriteBatch();
         stage = new Stage();
+        skin = new Skin();
         Gdx.input.setInputProcessor(stage);
+
         texture = new Texture(Gdx.files.internal("LiberationMono.png"), true); // true enables mipmaps
         texture.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear); // linear filtering in nearest mipmap image
         font = new BitmapFont(Gdx.files.internal("LiberationMono.fnt"), new TextureRegion(texture), false);
-
         font.setScale(1f, 1f);//scale to other devices - need to test it
-        skin = new Skin();
+
         buttonAtlas = new TextureAtlas("button.pack");
-        img = new Texture("penguin.png");
-        sprite = new Sprite(img);
+
+        iPeng = new Texture("penguin.png");
+        iGround = new Texture("ground.jpg");
+        sPeng = new Sprite(iPeng);
+        sGround = new Sprite(iGround);
         nWidth = Gdx.graphics.getWidth();
         nHeight = Gdx.graphics.getHeight();
-        // Center the sprite in the top/middle of the screen
-        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2);
 
-
-        world = new World(new Vector2(0, 0f), true);
+        world = new World(new Vector2(0, -9.8f), true);
+        // Penguin Sprite and Physics body
+        sPeng.setPosition(sPeng.getWidth(),sGround.getHeight());
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set((sprite.getX() + sprite.getWidth() / 2) /
-                        PIXELS_TO_METERS,
-                (sprite.getY() + sprite.getHeight() / 2) / PIXELS_TO_METERS);
+        bodyDef.position.set((sPeng.getX() + sPeng.getWidth() / 2) / fPM,
+                (sPeng.getY() + sPeng.getHeight() / 2) / fPM);
 
         body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(sprite.getWidth() / 2 / PIXELS_TO_METERS, sprite.getHeight()
-                / 2 / PIXELS_TO_METERS);
+        shape.setAsBox(sPeng.getWidth() / 2 / fPM, sPeng.getHeight()
+                / 2 / fPM);
 
 
-        shape.dispose();
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 10.0f;
+        fixtureDef.friction = 1.0f;
+        fixtureDef.restitution = 0f;
+        body.createFixture(fixtureDef);
+        //Ground body & sprite
+        sGround.setPosition(Gdx.graphics.getWidth() / 2 - sGround.getWidth() / 2, 0);
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set((sGround.getX() + sGround.getWidth() / 2) / fPM,
+                (sGround.getY() + sGround.getHeight() / 2) / fPM );
+        Body groundBody = world.createBody(groundBodyDef);
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox(sGround.getWidth() / 2 / fPM, sGround.getHeight()
+                / 2 / fPM);
+        groundBody.createFixture(groundBox, 0.0f);
+//
+        //Button
         skin.addRegions(buttonAtlas);
         textButtonStyle = new TextButton.TextButtonStyle();
         textButtonStyle.font = font;
@@ -83,7 +105,8 @@ public class jdjgame extends ApplicationAdapter {
         textButtonStyle.checked = skin.getDrawable("Button_Pressed");
         textButtonStyle.fontColor = Color.BLACK;
         button = new TextButton("LAUNCH", textButtonStyle);
-        button.setSize(nWidth / 6, nWidth / 6);
+        button.setSize(nWidth / 7, nWidth / 7);
+        button.setPosition(0,nHeight- nWidth/7);//Set it to top left corner
         stage.addActor(button);
         camera = new OrthographicCamera(nWidth, nHeight);
         button.addListener(new ChangeListener() {
@@ -93,6 +116,9 @@ public class jdjgame extends ApplicationAdapter {
                 body.setLinearVelocity(5f, 5f);
             }
         });
+
+
+        debugRenderer = new Box2DDebugRenderer();//For Debugging : Shows Boxes around the sprites
     }
 
 
@@ -100,24 +126,30 @@ public class jdjgame extends ApplicationAdapter {
     public void render() {
         camera.update();
         world.step(1 / 60f, 6, 2);
-        sprite.setPosition((body.getPosition().x * PIXELS_TO_METERS) - sprite.
+        sPeng.setPosition((body.getPosition().x * fPM) - sPeng.
                         getWidth() / 2,
-                (body.getPosition().y * PIXELS_TO_METERS) - sprite.getHeight() / 2)
+                (body.getPosition().y * fPM) - sPeng.getHeight() / 2)
         ;
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        debugMatrix = batch.getProjectionMatrix().cpy().scale(fPM,
+                fPM, 0);//For Debugging : Shows Boxes around the sprites
+
         batch.begin();
-        batch.draw(sprite, sprite.getX(), sprite.getY());
+        batch.draw(sPeng, sPeng.getX(), sPeng.getY());
+        batch.draw(sGround,sGround.getX(),sGround.getY());
         batch.end();
 
         stage.draw();
+
+        debugRenderer.render(world, debugMatrix);//For Debugging : Shows Boxes around the sprites
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        iPeng.dispose();
         world.dispose();
         texture.dispose();
         font.dispose();
