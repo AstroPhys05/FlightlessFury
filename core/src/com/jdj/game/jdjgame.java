f96f31e13eadd9ea0f67e598ab9a79188c984414
JoseRivas <jose.rivas.1997@gmail.com>
2014-11-20 13:32:53 -0500

    button colour update

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -43,6 +43,7 @@ public class jdjgame extends ApplicationAdapter {
     int nWidth, nHeight;
     Box2DDebugRenderer debugRenderer;
     Matrix4 debugMatrix;
+    //debugging
     @Override
     public void create() {
         batch = new SpriteBatch();
