754d508f847fe3d95b7e6d8ca48ab5a2f09ecf2c
JoseRivas <jose.rivas.1997@gmail.com>
2014-11-27 20:10:53 -0500

    SidesScrolling implemented

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -33,21 +33,22 @@ public class jdjgame extends ApplicationAdapter {
     final float fPM = 100f;//convert pixels to meters since box2d uses meters
     Stage stage;
     TextButton bLaunch, bReset;
+    Boolean bLaunchPressed = false;
     TextButton.TextButtonStyle textButtonStyle;
     Texture texture;
     BitmapFont font;
     Skin skin;
     TextureAtlas buttonAtlas;
     SpriteBatch batch;
-    Sprite sPeng, sGround;
-    Texture iPeng, iGround;
+    Sprite spPeng, spGround, spBg;
+    Texture iPeng, iGround, iBg;
     World world;
-    Body body;
+    Body body, groundBody;
     OrthographicCamera camera;
     int nWidth, nHeight;
     Box2DDebugRenderer debugRenderer;
     Matrix4 debugMatrix;
-
+    float scrollTimer = 0.0f;
     //debugging
     @Override
     public void create() {
@@ -55,7 +56,6 @@ public class jdjgame extends ApplicationAdapter {
         stage = new Stage();
         skin = new Skin();
         Gdx.input.setInputProcessor(stage);
-
         texture = new Texture(Gdx.files.internal("LiberationMono.png"), true); // true enables mipmaps
         texture.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear); // linear filtering in nearest mipmap image
         font = new BitmapFont(Gdx.files.internal("LiberationMono.fnt"), new TextureRegion(texture), false);
@@ -64,23 +64,29 @@ public class jdjgame extends ApplicationAdapter {
         buttonAtlas = new TextureAtlas("buttons.pack");
         iPeng = new Texture("penguin.png");
         iGround = new Texture("ground.jpg");
-        sPeng = new Sprite(iPeng);
-        sGround = new Sprite(iGround);
+        iBg = new Texture("city.jpg");
+        spPeng = new Sprite(iPeng);
+        spGround = new Sprite(iGround);
         nWidth = Gdx.graphics.getWidth();
         nHeight = Gdx.graphics.getHeight();
         world = new World(new Vector2(0, -9.8f), true);
-        sGround.setSize(nWidth, nHeight / 7);
+        spGround.setSize(nWidth*2, nHeight / 7/3);
+
+        iBg.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
+
+        spBg = new Sprite(iBg);
+        //spBg.setSize(nWidth*7,nHeight*7);
         // Penguin Sprite and Physics body
-        sPeng.setPosition(sPeng.getWidth(), sGround.getHeight());
+        spPeng.setPosition(spPeng.getWidth(), spGround.getHeight());
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set((sPeng.getX() + sPeng.getWidth() / 2) / fPM,
-                (sPeng.getY() + sPeng.getHeight() / 2) / fPM);
+        bodyDef.position.set((spPeng.getX() + spPeng.getWidth() / 2) / fPM,
+                (spPeng.getY() + spPeng.getHeight() / 2) / fPM);
 
         body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(sPeng.getWidth() / 2 / fPM, sPeng.getHeight()
+        shape.setAsBox(spPeng.getWidth() / 2 / fPM, spPeng.getHeight()
                 / 2 / fPM);
 
 
@@ -91,13 +97,13 @@ public class jdjgame extends ApplicationAdapter {
         fixtureDef.restitution = 0.5f;
         body.createFixture(fixtureDef);
         //Ground body & sprite
-        sGround.setPosition(Gdx.graphics.getWidth() / 2 - sGround.getWidth() / 2, 0);
+        spGround.setPosition(0, 0);
         BodyDef groundBodyDef = new BodyDef();
-        groundBodyDef.position.set((sGround.getX() + sGround.getWidth() / 2) / fPM,
-                (sGround.getY() + sGround.getHeight() / 2) / fPM);
-        Body groundBody = world.createBody(groundBodyDef);
+        groundBodyDef.position.set((spGround.getX() + spGround.getWidth() / 2) / fPM,
+                (spGround.getY() + spGround.getHeight() / 2) / fPM);
+        groundBody = world.createBody(groundBodyDef);
         PolygonShape groundBox = new PolygonShape();
-        groundBox.setAsBox(sGround.getWidth() / 2 / fPM, sGround.getHeight()
+        groundBox.setAsBox(spGround.getWidth() / 2 / fPM, spGround.getHeight()
                 / 2 / fPM);
         groundBody.createFixture(groundBox, 0.0f);
 //
@@ -116,14 +122,13 @@ public class jdjgame extends ApplicationAdapter {
         bLaunch.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                body.setLinearVelocity(2f, 20f);
+                bLaunchPressed = true;
                 return true;
             }
-//            @Override
-//            public void changed(ChangeEvent event, Actor actor) {
-//                //if bLaunch is pressed do stuff
-//                body.setLinearVelocity(5f,5f);
-//            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                bLaunchPressed = false;
+            }
         });
         //Button 2 : Reset
         bReset = new TextButton("RESET", textButtonStyle);
@@ -134,41 +139,61 @@ public class jdjgame extends ApplicationAdapter {
             public void changed(ChangeEvent event, Actor actor) {
                 body.setLinearVelocity(0, 0);
                 body.setAngularVelocity(0);
-                body.setTransform((sPeng.getWidth() + sPeng.getWidth() / 2) / fPM,
-                        (sGround.getHeight() + sPeng.getHeight() / 2) / fPM, 0);
+                body.setTransform((spPeng.getWidth() + spPeng.getWidth() / 2) / fPM,
+                        (spGround.getHeight() + spPeng.getHeight() / 2) / fPM, 0);
             }
 
         });
         stage.addActor(bLaunch);
         stage.addActor(bReset);
         debugRenderer = new Box2DDebugRenderer();//For Debugging : Shows Boxes around the sprites
+
     }
 
 
     @Override
     public void render() {
+        if(bLaunchPressed){
+            body.setLinearVelocity(5f,5f);
+        }
+        groundBody.setTransform(camera.position.x/fPM,groundBody.getPosition().y,0);
         world.step(1 / 60f, 6, 2);
-        sPeng.setPosition((body.getPosition().x * fPM) - sPeng.
+        spPeng.setPosition((body.getPosition().x * fPM) - spPeng.
                         getWidth() / 2,
-                (body.getPosition().y * fPM) - sPeng.getHeight() / 2)
+                (body.getPosition().y * fPM) - spPeng.getHeight() / 2)
         ;
-        sPeng.setRotation((float) Math.toDegrees(body.getAngle()));
+        spGround.setPosition((groundBody.getPosition().x * fPM) - spGround.
+                        getWidth() / 2,
+                (groundBody.getPosition().y * fPM) - spGround.getHeight() / 2)
+        ;
+        spPeng.setRotation((float) Math.toDegrees(body.getAngle()));
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         debugMatrix = batch.getProjectionMatrix().cpy().scale(fPM,
                 fPM, 0);//For Debugging : Shows Boxes around the sprites
 
-        camera.position.set(sPeng.getX(), sPeng.getY(), 0);
+        camera.position.set(spPeng.getX(), spGround.getY()+camera.viewportHeight/2, 0);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
+
+
+        scrollTimer += body.getLinearVelocity().x/(3*fPM);//May need to change the divisor to get a realistic sized velocity
+        if(scrollTimer>1.0f)
+           scrollTimer = 0.0f;
+
+        spBg.setU(scrollTimer);
+
+        spBg.setU2((scrollTimer+1));
+
         batch.begin();
-        batch.draw(sPeng, sPeng.getX(), sPeng.getY(), sPeng.getOriginX(),
-                sPeng.getOriginY(),
-                sPeng.getWidth(), sPeng.getHeight(), sPeng.getScaleX(), sPeng.
-                        getScaleY(), sPeng.getRotation());
-        batch.draw(sGround, sGround.getX(), sGround.getY(), sGround.getWidth(), sGround.getHeight());
+        batch.draw(spBg,camera.position.x-camera.viewportWidth/2,camera.position.y-camera.viewportHeight/2);
+        batch.draw(spPeng, spPeng.getX(), spPeng.getY(), spPeng.getOriginX(),
+                spPeng.getOriginY(),
+                spPeng.getWidth(), spPeng.getHeight(), spPeng.getScaleX(), spPeng.
+                        getScaleY(), spPeng.getRotation());
+        //batch.draw(spGround, spGround.getX(), spGround.getY(), spGround.getWidth(), spGround.getHeight());
         batch.end();
 
         stage.draw();
