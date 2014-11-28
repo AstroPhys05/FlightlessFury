b040e19cba26c04965453f7db43e97a59dae1c53
JoseRivas <jose.rivas.1997@gmail.com>
2014-11-28 13:54:40 -0500

    HandlingInput

--- core/src/com/jdj/game/jdjgame.java
+++ core/src/com/jdj/game/jdjgame.java
@@ -1,235 +1,80 @@
+// Text Input
+//Source: http://tutorial-libgdx-android.blogspot.ca/2014/02/handling-inputs-text.html
+//https://github.com/libgdx/libgdx/wiki/File-handling#writing-to-a-file
 package com.jdj.game;
-//TODO clean up code by making multiple files and grouping similar code
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+
+import com.badlogic.gdx.Input.TextInputListener;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.math.Matrix4;
-//SideScrolling
-//https://code.google.com/p/libgdx-users/wiki/ScrollingTexture
 
-//Box2d Physics
-//http://gamedevelopment.tutsplus.com/tutorials/how-to-create-a-custom-2d-physics-engine-the-basics-and-impulse-resolution--gamedev-6331
-//https://github.com/libgdx/libgdx/wiki/Box2d
-//https://www.youtube.com/watch?v=IDZMRDb1A_M#t=812
-//http://programmersweb.blogspot.ca/2012/07/simple-libgdx-box2d-bouncing-ball.html
+public class jdjgame implements ApplicationListener {
 
-//Buttons
-//http://stackoverflow.com/questions/21488311/libgdx-how-to-create-a-button
-//https://github.com/libgdx/libgdx/wiki/Texture-packer
-//http://gamedev.stackexchange.com/questions/60123/registering-inputlistener-in-libgdx
+    private SpriteBatch batch ;
+    private BitmapFont font ;
 
-//OrthoCam
-//http://www.gamefromscratch.com/post/2013/11/06/LibGDX-Tutorial-7-Camera-basics.aspx
-//https://github.com/libgdx/libgdx/wiki/Orthographic-camera
-public class jdjgame extends ApplicationAdapter {
-    final float fPM = 100f;//convert pixels to meters since box2d uses meters
-    Stage stage;
-    TextButton bLaunch, bReset;
-    Boolean bLaunchPressed = false;
-    TextButton.TextButtonStyle textButtonStyle;
-    Texture texture;
-    BitmapFont font;
-    Skin skin;
-    TextureAtlas buttonAtlas;
-    SpriteBatch batch;
-    Sprite spPeng, spGround, spBg;
-    Texture iPeng, iGround, iBg;
-    World world;
-    Body body, groundBody;
-    OrthographicCamera camera;
-    int nWidth, nHeight;
-    Box2DDebugRenderer debugRenderer;
-    Matrix4 debugMatrix;
-    float scrollTimer = 0.0f;
-    //debugging
+    private String sInitial;
+    private String sDialogue;
+    private String sMessage;
+    // FileHandle file = Gdx.files.external("file.txt");
+    private boolean afficher;
     @Override
     public void create() {
+
+
+        sInitial = "Your name:";
+        sDialogue = "Flightless Fury";
+        afficher = false;
+
         batch = new SpriteBatch();
-        stage = new Stage();
-        skin = new Skin();
-        Gdx.input.setInputProcessor(stage);
-        texture = new Texture(Gdx.files.internal("LiberationMono.png"), true); // true enables mipmaps
-        texture.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear); // linear filtering in nearest mipmap image
-        font = new BitmapFont(Gdx.files.internal("LiberationMono.fnt"), new TextureRegion(texture), false);
-        font.setScale(1f, 1f);//scale to other devices - need to test it
+        font = new BitmapFont();
 
-        buttonAtlas = new TextureAtlas("buttons.pack");
-        iPeng = new Texture("penguin.png");
-        iGround = new Texture("ground.jpg");
-        iBg = new Texture("city.jpg");
-        spPeng = new Sprite(iPeng);
-        spGround = new Sprite(iGround);
-        nWidth = Gdx.graphics.getWidth();
-        nHeight = Gdx.graphics.getHeight();
-        world = new World(new Vector2(0, -9.8f), true);
-        spGround.setSize(nWidth*2, nHeight / 7/3);
+        Gdx.input.getTextInput(new TextInputListener() {
 
-        iBg.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
-
-        spBg = new Sprite(iBg);
-        //spBg.setSize(nWidth*7,nHeight*7);
-        // Penguin Sprite and Physics body
-        spPeng.setPosition(spPeng.getWidth(), spGround.getHeight());
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set((spPeng.getX() + spPeng.getWidth() / 2) / fPM,
-                (spPeng.getY() + spPeng.getHeight() / 2) / fPM);
-
-        body = world.createBody(bodyDef);
-
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(spPeng.getWidth() / 2 / fPM, spPeng.getHeight()
-                / 2 / fPM);
-
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 10.0f;
-        fixtureDef.friction = 0.3f;
-        fixtureDef.restitution = 0.5f;
-        body.createFixture(fixtureDef);
-        //Ground body & sprite
-        spGround.setPosition(0, 0);
-        BodyDef groundBodyDef = new BodyDef();
-        groundBodyDef.position.set((spGround.getX() + spGround.getWidth() / 2) / fPM,
-                (spGround.getY() + spGround.getHeight() / 2) / fPM);
-        groundBody = world.createBody(groundBodyDef);
-        PolygonShape groundBox = new PolygonShape();
-        groundBox.setAsBox(spGround.getWidth() / 2 / fPM, spGround.getHeight()
-                / 2 / fPM);
-        groundBody.createFixture(groundBox, 0.0f);
-//
-        //Button 1 : Launch
-        skin.addRegions(buttonAtlas);
-        textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.font = font;
-        textButtonStyle.up = skin.getDrawable("Launch_Pressed");
-        textButtonStyle.down = skin.getDrawable("Launch_Unpressed");
-        textButtonStyle.checked = skin.getDrawable("Launch_Pressed");
-        textButtonStyle.font.setColor(Color.WHITE);
-        bLaunch = new TextButton("LAUNCH", textButtonStyle);
-        bLaunch.setSize(nWidth / 7, nWidth / 7);
-        bLaunch.setPosition(0, nHeight - nWidth / 7);//Set it to top left corner
-        camera = new OrthographicCamera(nWidth, nHeight);
-        camera.position.y = spGround.getY()+camera.viewportHeight/2;
-        bLaunch.addListener(new InputListener() {
             @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                bLaunchPressed = true;
-                return true;
-            }
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                bLaunchPressed = false;
-            }
-        });
-        //Button 2 : Reset
-        bReset = new TextButton("RESET", textButtonStyle);
-        bReset.setSize(nWidth / 7, nWidth / 7);
-        bReset.setPosition(nWidth / 7, nHeight - nWidth / 7);
-        bReset.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                body.setLinearVelocity(0, 0);
-                body.setAngularVelocity(0);
-                body.setTransform((spPeng.getWidth() + spPeng.getWidth() / 2) / fPM,
-                        (spGround.getHeight() + spPeng.getHeight() / 2) / fPM, 0);
-                camera.position.y = spGround.getY()+camera.viewportHeight/2;
-                scrollTimer = 0f;
+            public void input(String sName) {
+                sMessage = "Hello, <"+sName+"> Welcome Flightless Fury  ";
+                // file.writeString("welcome",true);
+                afficher =true;
             }
 
-        });
-        stage.addActor(bLaunch);
-        stage.addActor(bReset);
-        debugRenderer = new Box2DDebugRenderer();//For Debugging : Shows Boxes around the sprites
-
-    }
-
-
-    @Override
-    public void render() {
-        if(bLaunchPressed){
-            body.setLinearVelocity(20f,10f);
-        }
-        groundBody.setTransform(camera.position.x/fPM,groundBody.getPosition().y,0);
-        world.step(1 / 60f, 6, 2);
-        spPeng.setPosition((body.getPosition().x * fPM) - spPeng.
-                        getWidth() / 2,
-                (body.getPosition().y * fPM) - spPeng.getHeight() / 2)
-        ;
-        spGround.setPosition((groundBody.getPosition().x * fPM) - spGround.
-                        getWidth() / 2,
-                (groundBody.getPosition().y * fPM) - spGround.getHeight() / 2)
-        ;
-        spPeng.setRotation((float) Math.toDegrees(body.getAngle()));
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        debugMatrix = batch.getProjectionMatrix().cpy().scale(fPM,
-                fPM, 0);//For Debugging : Shows Boxes around the sprites
-        camera.position.x = spPeng.getX()+ spPeng.getWidth()/2;
-        camera.position.y= spPeng.getY()+spPeng.getHeight()/2;
-        if(camera.position.y<=spGround.getY()+camera.viewportHeight/2){
-            camera.position.y = spGround.getY()+camera.viewportHeight/2;
-        }
-        //camera.position.set(spPeng.getX(), spGround.getY()+camera.viewportHeight/2, 0);
-
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
-
-
-        scrollTimer += body.getLinearVelocity().x/(1000);//May need to change the divisor to get a realistic sized velocity
-        if(scrollTimer>1.0f)
-           scrollTimer = 0.0f;
-
-        spBg.setU(scrollTimer);
-
-        spBg.setU2((scrollTimer+1));
-
-        batch.begin();
-        batch.draw(spBg,camera.position.x-camera.viewportWidth/2,groundBody.getPosition().y);
-        batch.draw(spPeng, spPeng.getX(), spPeng.getY(), spPeng.getOriginX(),
-                spPeng.getOriginY(),
-                spPeng.getWidth(), spPeng.getHeight(), spPeng.getScaleX(), spPeng.
-                        getScaleY(), spPeng.getRotation());
-        //batch.draw(spGround, spGround.getX(), spGround.getY(), spGround.getWidth(), spGround.getHeight());
-        batch.end();
-
-        stage.draw();
-
-        debugRenderer.render(world, debugMatrix);//For Debugging : Shows Boxes around the sprites
+            @Override
+            public void canceled() {
+                sMessage = "I guess you don't want to play..";
+                afficher =true;
+            }
+        }, sDialogue, sInitial);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        iPeng.dispose();
-        world.dispose();
-        texture.dispose();
-        font.dispose();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void render() {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(afficher)
+        {
+            batch.begin();
+            font.draw(batch, sMessage, 10, 200);
+            batch.end();
+        }
+    }
+
+    @Override
+    public void resize(int arg0, int arg1) {
+    }
+
+    @Override
+    public void resume() {
     }
 }
