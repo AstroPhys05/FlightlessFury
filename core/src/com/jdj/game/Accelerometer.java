0cadd7db13ac1b9d74374bd98657ad66a7d1f4aa
AstroPhys05 <jashpandya@hotmail.com>
2014-12-11 14:23:59 -0500

    Angle changed when phone is tilted

--- core/src/com/jdj/game/Accelerometer.java
+++ core/src/com/jdj/game/Accelerometer.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.Input;
 /**
  * Created by Jash on 04/12/2014.
  */
-////https://github.com/libgdx/libgdx/wiki/Accelerometer
+
 public class Accelerometer {
     public static float accelY(){
-        return Gdx.input.getAccelerometerY();
+        return Gdx.input.getAccelerometerY();                      // https://github.com/libgdx/libgdx/wiki/Accelerometer
     }
 }
+
+//We used to have the Up and Down buttons rotating the penguin
+//but after thinking of using the accelerometer, we used the accelerometer.
