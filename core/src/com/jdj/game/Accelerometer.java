fcdfddf79c1774278f2c5a40008f0386b0ec8c2d
JoseRivas <jose.rivas.1997@gmail.com>
2014-12-11 14:22:55 -0500

    penguin rotates using accelerometer

--- core/src/com/jdj/game/Accelerometer.java
+++ core/src/com/jdj/game/Accelerometer.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.Input;
 /**
  * Created by Jash on 04/12/2014.
  */
-////https://github.com/libgdx/libgdx/wiki/Accelerometer
+//https://github.com/libgdx/libgdx/wiki/Accelerometer
 public class Accelerometer {
     public static float accelY(){
         return Gdx.input.getAccelerometerY();
